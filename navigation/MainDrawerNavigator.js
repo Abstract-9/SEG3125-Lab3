ee39886ac11dddc50a7b899fff97892d381b05fc
jfan <sleepiesheepy@gmail.com>
2019-07-01 18:35:54 -0400

    weeaer

--- navigation/MainDrawerNavigator.js
+++ navigation/MainDrawerNavigator.js
@@ -1,33 +1,18 @@
 import React from 'react';
-import { Platform } from 'react-native';
 import {
   createStackNavigator,
   createDrawerNavigator
 } from 'react-navigation';
 
-import HomeScreen from '../screens/HomeScreen';
 import indexScreen from '../screens/indexScreen'
-import MenuScreen from '../screens/MenuScreen'
 import RestaurantListScreen from '../screens/ResturantListScreen'
-import CheckoutScreen from '../screens/CheckoutScreen'
 import OrderScreen from '../screens/OrderScreen'
+
 
 const home_stackNav = createStackNavigator({
   Home: {
-    screen: HomeScreen,
-  }
-});
-
-const index_stackNav = createStackNavigator({
-  Index: {
     screen: indexScreen
   }
-});
-
-const menu_stackNav = createStackNavigator({
-    Menu: {
-        screen: MenuScreen
-    }
 });
 
 const restaurant_stackNav = createStackNavigator({
@@ -42,31 +27,20 @@ const order_stackNav = createStackNavigator({
     }
 });
 
-const checkout_stackNav = createStackNavigator({
-    Checkout: {
-        screen: CheckoutScreen,
-    }
-});
 
 export default createDrawerNavigator({
+
       Home: {
-        screen: home_stackNav
+          screen: home_stackNav
       },
-      Index: {
-        screen: index_stackNav
-      },
-      Menu: {
-          screen: menu_stackNav
-      },
+
       Restaurant: {
           screen: restaurant_stackNav,
       },
       Order: {
-        screen: order_stackNav,
+          screen: order_stackNav,
       },
-      Checkout: {
-          screen: checkout_stackNav,
-        }
+
     }
 )
 
