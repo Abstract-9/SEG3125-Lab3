8242673b628e44c1998897d83f1619da62912c58
jfan <sleepiesheepy@gmail.com>
2019-07-01 14:18:09 -0400

    weoweo

---
+++ screens/indexScreen.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { AppRegistry, View } from 'react-native';
+
+export default class indexScreen extends Component {
+    render() {
+        return (
+            // Try setting `alignItems` to 'flex-start'
+            // Try setting `justifyContent` to `flex-end`.
+            // Try setting `flexDirection` to `row`.
+            <View style={{
+                flex: 1,
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'stretch',
+            }}>
+                <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+                <View style={{height: 50, backgroundColor: 'skyblue'}} />
+                <View style={{height: 100, backgroundColor: 'steelblue'}} />
+            </View>
+        );
+    }
+};
+
+
